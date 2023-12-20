f50b6fabafb86ecf24fa59343968d087347313f1
Wayne Ritchie <rockin4life33@gmail.com>
2023-12-20 17:24:33 -0600

    Updated the Popover (dropdown) for the NavBar.

--- app/page.tsx
+++ app/page.tsx
@@ -38,8 +38,8 @@ export default async function Home() {
         <Card
           key="TTSNextTpl"
           title="TechTastic Solutions Next.js Tpl"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore      pariatur quis molestiae fugiat illo, praesentium asperiores impedit inventore ipsa."
-          demo={`asdf Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore pariatur quis molestiae fugiat illo, praesentium asperiores impedit inventore ipsa. Deleniti atque, itaque delectus saepe eaque sed aperiam maiores nulla. asdf Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nobis quis architecto! Quisquam fuga ea corrupti, animi excepturi consequatur suscipit dolorem, repellendus veniam dolore beatae consequuntur nisi quas aperiam modi. asdf Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus consequuntur non quia vero nulla temporibus molestias numquam recusandae. Consectetur ea velit, minus hic quis fuga pariatur aliquid placeat magni eius? asdf Lorem ipsum dolor amet consectetur adipisicing elit. Ipsum hic, accusantium perferendis excepturi doloribus, beatae mollitia sunt nulla quis similique corrupti fugit temporibus deserunt! Reiciendis, voluptatum! asdf`}
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore pariatur quis molestiae fugiat illo, praesentium asperiores impedit inventore ipsa."
+          demo={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore pariatur quis molestiae fugiat illo, praesentium asperiores impedit inventore ipsa. Deleniti atque, itaque delectus saepe eaque sed aperiam maiores nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nobis quis architecto! Quisquam fuga ea corrupti, animi excepturi consequatur suscipit dolorem, repellendus veniam dolore beatae consequuntur nisi quas aperiam modi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus consequuntur non quia vero nulla temporibus molestias numquam recusandae. Consectetur ea velit, minus hic quis fuga pariatur aliquid placeat magni eius?`}
         />
       </div>
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
@@ -102,7 +102,7 @@ const features: tFeatures[] = [
   {
     title: "Built-in Auth + Database",
     description:
-      "Precedent comes with authentication and database via [Auth.js](https://authjs.dev/) + [Prisma](https://prisma.io/)",
+      "TechTastic Solutions Precedent template comes with authentication and database via [Auth.js](https://authjs.dev/) + [Prisma](https://prisma.io/)",
     demo: (
       <div className="flex items-center justify-center space-x-20">
         <Image alt="Auth.js logo" src="/authjs.webp" width={50} height={50} />
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,9 +6,13 @@ import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import Popover from "../shared/popover";
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
+  const [openPopover, setOpenPopover] = useState(false);
   const scrolled = useScroll(50);
 
   return (
@@ -17,8 +21,8 @@ export default function NavBar({ session }: { session: Session | null }) {
       <div
         className={`fixed top-0 flex w-full justify-center ${
           scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
+            ? "border-b border-gray-300 bg-white/50 backdrop-blur-xl"
+            : "border-b border-gray-100 bg-white/0"
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
@@ -26,10 +30,10 @@ export default function NavBar({ session }: { session: Session | null }) {
             <Image
               src="/logo.png"
               alt="TechTastic Solutions logo"
-              width="33"
-              height="33"
+              width="30"
+              height="30"
               className="mr-2 rounded-md"
-            ></Image>
+            />
             <p>TechTastic Solutions</p>
           </Link>
           <div>
@@ -45,6 +49,36 @@ export default function NavBar({ session }: { session: Session | null }) {
             >
               Contact
             </Link>
+            <Popover
+              content={
+                <div className="w-full rounded-md bg-white p-2 sm:w-40">
+                  <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
+                    Item 1
+                  </button>
+                  <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
+                    Item 2
+                  </button>
+                  <button className="flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200">
+                    Item 3
+                  </button>
+                </div>
+              }
+              openPopover={openPopover}
+              setOpenPopover={setOpenPopover}
+            >
+              <button
+                onClick={() => setOpenPopover(!openPopover)}
+                className="text-md mr-1 rounded border border-black bg-black p-1 px-4 text-white transition-all hover:bg-white hover:text-black"
+              >
+                Popover
+                <ChevronDown
+                  className={`h-6 w-7 pl-2 transition-all ${
+                    openPopover ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
+            </Popover>
+
             <Link
               href="/about"
               className="mr-1 items-center rounded border border-black bg-black p-1.5 px-4 font-display text-lg text-white transition-all hover:bg-white hover:text-black"
